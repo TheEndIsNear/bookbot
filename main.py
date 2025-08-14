d4b585eba42ef80156b0abc724751ad7d52c0f0c
Matt <2172212+TheEndIsNear@users.noreply.github.com>
2025-08-14 17:57:01 -0600

    print out a final report for the book

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
-from stats import get_book_text, get_word_count
+from stats import get_book_text, get_word_count, get_char_frequency, sorted_char_frequency
 def main():
-    book_text = get_book_text('./books/frankenstein.txt')
+    book_filepath = './books/frankenstein.txt'
+    book_text = get_book_text(book_filepath)
     wc = get_word_count(book_text)
-    print(f"{wc} words found in the document")
+    char_dict = get_char_frequency(book_text)
+    sorted_char_dict = sorted_char_frequency(char_dict)
+    print_report(book_filepath, wc, sorted_char_dict)
+
+def print_report(book_filepath, wc, char_dict):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_filepath}...")
+    print("----------- Word Count ----------")
+    print(f"Found {wc} total words")
+    print("--------- Character Count -------")
+    for key, value in char_dict.items():
+        if key.isalpha():
+            print(f"{key}: {value}")
 
 main()
--- stats.py
+++ stats.py
@@ -2,5 +2,22 @@ def get_book_text(filepath):
     with open(filepath) as f:
         return f.read()
 
+def get_words(book):
+    return book.split()
+
 def get_word_count(book):
-    return len(book.split())
+    return len(get_words(book))
+
+def get_char_frequency(book):
+    dict = {}
+    for word in get_words(book):
+        chars = list(word)
+        for char in chars:
+            lower= char.lower()
+            count = dict.get(lower, 0)
+            count+= 1
+            dict[lower] = count
+    return dict
+
+def sorted_char_frequency(char_dict):
+    return dict(sorted(char_dict.items(), key=lambda item: item[1],reverse=True))
